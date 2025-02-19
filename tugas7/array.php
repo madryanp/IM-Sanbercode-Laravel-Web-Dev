8449b21b0d5ded57e1fc00897cea3f1096d01dca
madryanp <madrianpratama@gmail.com>
2025-02-19 13:43:54 +0700

    commit tugas 7

---
+++ tugas7/array.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array</title>
+</head>
+
+<body>
+    <h1>Berlatih Array</h1>
+
+    <?php
+    echo "<h3> Soal 1 </h3>";
+    /* 
+            SOAL NO 1
+            Kelompokkan nama-nama di bawah ini ke dalam Array.
+            Kids : "Mike", "Dustin", "Will", "Lucas", "Max", "Eleven" 
+            Adults: "Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"
+        */
+    $kids = ["mike", "dustin", "thio", "abdul"];  // Lengkapi di sini 
+    $adults = ["hopper", "nantcy", "joyce", "jonatchan", "murray"];
+    echo " <br> kids : " . print_r($kids) . "<br>";
+    echo " <br> adult : " . print_r($adults) . "<br>";
+
+    echo "<h3> Soal 2</h3>";
+    /* 
+            SOAL NO 2
+            Tunjukkan panjang Array di Soal No 1 dan tampilkan isi dari masing-masing Array.
+        */
+    echo "Cast Stranger Things: ";
+    echo "<br>";
+    echo "Total Kids: " . count($kids); // Berapa panjang array kids
+    echo "<br>";
+    echo "<ol>";
+    echo "<li> $kids[0] </li>";
+    echo "<li> $kids[1] </li>";
+    echo "<li> $kids[2] </li>";
+    echo "<li> $kids[3] </li>";
+    
+    // Lanjutkan
+
+    echo "</ol>";
+
+    echo "Total Adults: " . count($adults); // Berapa panjang array adults
+    echo "<br>";
+    echo "<ol>";
+    echo "<li> $adults[0] </li>";
+    echo "<li> $adults[1] </li>";
+    echo "<li> $adults[2] </li>";
+    echo "<li> $adults[3] </li>";
+    echo "<li> $adults[4] </li>";
+  
+    // Lanjutkan
+
+    echo "</ol>";
+
+    /*
+            SOAL No 3
+            Susun data-data berikut ke dalam bentuk Asosiatif Array didalam Array Multidimensi
+            
+            Name: "Will Byers"
+            Age: 12,
+            Aliases: "Will the Wise"
+            Status: "Alive"
+
+            Name: "Mike Wheeler"
+            Age: 12,
+            Aliases: "Dungeon Master"
+            Status: "Alive"
+
+            Name: "Jim Hopper"
+            Age: 43,
+            Aliases: "Chief Hopper"
+            Status: "Deceased"
+
+            Name: "Eleven"
+            Age: 12,
+            Aliases: "El"
+            Status: "Alive"
+
+
+            Output:
+            Array
+                (
+                    [0] => Array
+                        (
+                            [Name] => Will Byers
+                            [Age] => 12
+                            [Aliases] => Will the Wise
+                            [Status] => Alive
+                        )
+
+                    [1] => Array
+                        (
+                            [Name] => Mike Wheeler
+                            [Age] => 12
+                            [Aliases] => Dugeon Master
+                            [Status] => Alive
+                        )
+
+                    [2] => Array
+                        (
+                            [Name] => Jim Hooper
+                            [Age] => 43
+                            [Aliases] => Chief Hopper
+                            [Status] => Deceased
+                        )
+
+                    [3] => Array
+                        (
+                            [Name] => Eleven
+                            [Age] => 12
+                            [Aliases] => El
+                            [Status] => Alive
+                        )
+
+                )
+            
+        */
+
+        echo "<h3> Soal 3</h3> <br>";
+        $poeple = [ 
+        ["name" => "Adryan", "age" => "25", "Aliases"=> "ad", "Status"=> "Alive"],
+        ["name" => "lolo", "age" => "25", "Aliases"=> "lo", "Status"=> "Alive"],
+        ["name" => "jojo", "age" => "25", "Aliases"=> "jo", "Status"=> "Alive"],
+        ["name" => "elsa", "age" => "25", "Aliases"=> "el", "Status"=> "Alive"],
+        
+];
+
+echo "<pre>";
+print_r($poeple);
+echo "</pre>";
+    ?>
+</body>
+
+</html>
